ae44a5fe69c5d2d9959b50218a7957c6c7a14b16
Inigo Lopez <ilopez@alienvault.com>
2019-05-20 18:00:42 +0200

    Black format

--- ioc_collector.py
+++ ioc_collector.py
@@ -9,15 +9,19 @@ from OTXv2 import OTXv2
 from OTXv2 import IndicatorTypes
 
 
-class IOCCollector():
-
+class IOCCollector:
     def __init__(self, otx_key, otx_users):
         self.otx = OTXv2(otx_key)
         self.users = otx_users
 
     def get_user_iocs(self, user):
-        iocs = [ioc['indicator'] for ioc in \
-                self.otx.get_all_indicators(author_name=user, indicator_types=[IndicatorTypes.DOMAIN, IndicatorTypes.HOSTNAME])]
+        iocs = [
+            ioc["indicator"]
+            for ioc in self.otx.get_all_indicators(
+                author_name=user,
+                indicator_types=[IndicatorTypes.DOMAIN, IndicatorTypes.HOSTNAME],
+            )
+        ]
         return iocs
 
     def get_all_iocs(self):
@@ -27,8 +31,7 @@ class IOCCollector():
         return sorted(list(set(iocs)))
 
 
-class GitHubUpdater():
-
+class GitHubUpdater:
     def __init__(self):
         with open("config.json", "r") as f:
             self.cfg = json.load(f)
@@ -44,14 +47,19 @@ class GitHubUpdater():
     def update_iocs(self):
         iocs = self.ioc_collector.get_all_iocs()
         print(f"Got {len(iocs)} IOCs from OTX")
-        iocs_b64 = base64.b64encode("\n".join(self.ioc_collector.get_all_iocs()).encode()).decode()
+        iocs_b64 = base64.b64encode(
+            "\n".join(self.ioc_collector.get_all_iocs()).encode()
+        ).decode()
 
-        headers = {'Authorization': f"token {self.cfg['github_key']}"}
-        payload = {"message": f"IOCs updated on {datetime.now()}",
-                   "content": iocs_b64,
-                   "sha": self._get_current_sha()}
-        r = requests.put(self.cfg["github_update_path"], data=json.dumps(payload),
-                         headers = headers)
+        headers = {"Authorization": f"token {self.cfg['github_key']}"}
+        payload = {
+            "message": f"IOCs updated on {datetime.now()}",
+            "content": iocs_b64,
+            "sha": self._get_current_sha(),
+        }
+        r = requests.put(
+            self.cfg["github_update_path"], data=json.dumps(payload), headers=headers
+        )
         print(r.json())
 
 
